92338d8dff08b524e80f9d6919a5a9a1adf0776b
oleg <sad.net79@gmail.com>
2020-08-16 20:33:30 +0300

    version > -- 3.1.9

--- script.php
+++ script.php
@@ -1,5 +1,7 @@
 <?php
 // No direct access to this file
+use Joomla\CMS\Factory;
+
 defined('_JEXEC') or die;
 
 /**
@@ -67,6 +69,58 @@ class mod_rokajaxsearchInstallerScript
      */
     function preflight($typeExt, $parent)
     {
+        // manifest file version
+        $this->release = $parent->get('manifest')->version;
+        $this->minimum_version_gnz11 = $parent->get('manifest')->minimum_version_gnz11;
+
+        $VersionGnz11 = $this->getVersionGnz11();
+        
+        echo'<pre>';print_r( $VersionGnz11 );echo'</pre>'.__FILE__.' '.__LINE__;
+        echo'<pre>';print_r( $parent );echo'</pre>'.__FILE__.' '.__LINE__;
+
+        
+        if (version_compare( $VersionGnz11 , $this->minimum_version_gnz11, '<'))
+        {
+            die(__FILE__ .' '. __LINE__ );
+            
+        }
+        die(__FILE__ .' '. __LINE__ );
+        
+        # Отменить, если устанавливаемый модуль не новее, чем текущая установленная версия
+        # Abort if the module being installed is not newer than the currently installed version
+        if (strtolower($typeExt) === 'update')
+        {
+            $manifest   = $this->getItemArray( Factory::getDbo()->quote($this->extension));
+            $oldRelease = $manifest['version'];
+
+            # Кто-то пытается установить более раннюю версию, чем установлена сейчас
+            # Someone is trying to install a lower version than is currently installed
+            if (version_compare($this->release, $oldRelease, '<'))
+            {
+                # Выдать сообщение об ошибке и вернуть false
+                # Throw some error message and return false
+                JApplicationCms::getInstance('site')->enqueueMessage('Error msg' , 'error' ) ;
+
+                return false;
+            }
+
+            # Устанавливаемая версия выше той, которая установлена в настоящее время
+            # The version being installed is higher than what is currently installed
+            if (version_compare($oldRelease, $this->release, '<'))
+            {
+                # Здесь вы можете выполнить функцию
+                # You can execute a function here
+
+                # Если вы хотите сравнить конкретную версию.
+                # If you want to compare a specific version.
+                if (version_compare($oldRelease, '2.0.0', '<='))
+                {
+                    # Функция обновления для версии 2.0.0
+                    # Update function for version 2.0.0
+                    # $this->updateToVersionTwo();
+                }
+            }
+        }
         JLoader::registerNamespace( 'GNZ11' , JPATH_LIBRARIES . '/GNZ11' , $reset = false , $prepend = false , $type = 'psr4' );
         \GNZ11\Extensions\ScriptFile::updateProcedure($typeExt, $parent);
     }
@@ -86,6 +140,31 @@ class mod_rokajaxsearchInstallerScript
     function postflight($type, $parent)
     {
 //        echo '<p>Anything here happens after the installation/update/uninstallation of the module.</p>';
+    }
+
+    /**
+     * Получить версию library Gnz11
+     * @return mixed
+     * @since 3.9
+     */
+    protected function getVersionGnz11(){
+        $manifest   = $this->getItemArray( Factory::getDbo()->quote('GNZ11'));
+        return $manifest['version'];
+    }
+    protected function getItemArray($identifier)
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true)
+            ->select($db->qn('manifest_cache'))
+            ->from($db->qn('#__extensions'))
+            ->where($db->qn('element') . ' = ' . $identifier);
+        $db->setQuery($query);
+
+        // Load the single cell and json_decode data
+        $array = json_decode($db->loadResult(), true);
+
+        return $array;
     }
 
 
