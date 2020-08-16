730c00e64f016acdf8c98e27898b3f7a258d1d01
oleg <sad.net79@gmail.com>
2020-08-16 21:06:27 +0300

    version > -- 3.1.9

--- script.php
+++ script.php
@@ -71,23 +71,9 @@ class mod_rokajaxsearchInstallerScript
     {
         // manifest file version
         $this->release = $parent->get('manifest')->version;
-        foreach ( $parent->get('manifest')->version_gnz11 as $v )
-        {
-            $this->minimum_version_gnz11 = $v ; 
-            echo'<pre>';print_r( (string)$v );echo'</pre>'.__FILE__.' '.__LINE__;
-
-
-        }#END FOREACH
-//        $this->minimum_version_gnz11 = $parent->get('manifest')->version_gnz11[0];
-
+        $this->minimum_version_gnz11 = (string)$parent->get('manifest')->version_gnz11 ;
         $VersionGnz11 = $this->getVersionGnz11();
-        
-        echo'<pre>';print_r( (string)$parent->get('manifest')->version_gnz11 );echo'</pre>'.__FILE__.' '.__LINE__;
-        echo'<pre>';print_r( $parent->get('manifest') );echo'</pre>'.__FILE__.' '.__LINE__;
-        echo'<pre>';print_r( $this->release );echo'</pre>'.__FILE__.' '.__LINE__;
-        echo'<pre>';print_r( $VersionGnz11 );echo'</pre>'.__FILE__.' '.__LINE__;
-        echo'<pre>';print_r( $this->minimum_version_gnz11 );echo'</pre>'.__FILE__.' '.__LINE__;
-die(__FILE__ .' '. __LINE__ );
+
 
         
         if (version_compare( $this->minimum_version_gnz11 , $VersionGnz11  , '<'))
